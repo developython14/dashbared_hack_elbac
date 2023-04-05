dc5a6292c4f248add2b3e0a1adca3b90758a0ee3
developython14 <djmel1955@gmail.com>
2023-04-05 16:32:46 +0200

    work on admin add pub in admin

--- lib/screens/landing/Home/home.dart
+++ lib/screens/landing/Home/home.dart
@@ -248,6 +248,92 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Future<void> _dialogBuilder_editpublicite(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('تحرير القصص'),
+          content: Form(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              DropdownButton(
+                  hint: Text(
+                    'chose role',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  isExpanded: true,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text('اختر القصص التي تريد تخصيصها '),
+                      value: 'expert',
+                    ),
+                    DropdownMenuItem(
+                      child: Text('freede'),
+                      value: 'user',
+                    )
+                  ],
+                  onChanged: (value) {}),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'اسم جديد ستوري'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              GestureDetector(
+                onTap: () async {
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(allowMultiple: true);
+
+                  if (result != null) {
+                    setState(() {});
+                  } else {
+                    // User canceled the picker
+                  }
+                },
+                child: Text('انقر هنا لإضافة ملفات'),
+              ),
+            ],
+          )),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('غلق'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('إضافة '),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                  backgroundColor: Colors.red),
+              child: const Text('حذف هذه القصص '),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var items = [
@@ -321,14 +407,14 @@ class _HomeState extends State<Home> {
             children: [
               ElevatedButton(
                   onPressed: () {
-                    _dialogBuilder_add_stories(context);
+                    _dialogBuilder_adpublicite(context);
                   },
                   child: Text('إضافة إعلانات جديدة')),
               ElevatedButton(
                   onPressed: () {
-                    _dialogBuilder_add_files_to_Stories(context);
+                    _dialogBuilder_editpublicite(context);
                   },
-                  child: Text('إضافة عنصر إلى القصص')),
+                  child: Text(' تحرير في الإعلان ')),
             ],
           ),
           levels_componant(
