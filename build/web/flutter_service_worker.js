34fb214c094fef9ca8478c2b2a1584b4224711c3
developython14 <djmel1955@gmail.com>
2023-04-07 01:21:06 +0200

    add second verison99

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,8 +32,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0cea0b507fe99f8dd5caf25be5f821a8",
-"/": "0cea0b507fe99f8dd5caf25be5f821a8",
+"index.html": "9a82b6a48f979454dc7b8a68e8d245c7",
+"/": "9a82b6a48f979454dc7b8a68e8d245c7",
 "main.dart.js": "f976a75adf71b550a368205705005c53",
 "manifest.json": "1a67056ee4416d3b6a5a8eb7b4e55439",
 "version.json": "429a7cc8ad1dcaa60b7c4e2d69f51034"
