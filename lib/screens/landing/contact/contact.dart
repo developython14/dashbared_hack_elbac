668bad20a23808b525919763c34121c6a05a4bd3
developython14 <djmel1955@gmail.com>
2023-04-10 16:48:18 +0200

    work on admin cotact page

--- lib/screens/landing/contact/contact.dart
+++ lib/screens/landing/contact/contact.dart
@@ -2,46 +2,108 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/foundation.dart';
 
-final Uri _url_email =
-    Uri.parse('mailto: S7QURAN-S@hotmail.com?subject=help&body=New%question');
-final Uri website = Uri.parse('https://rightservices.herokuapp.com/');
-final Uri _url_phone = Uri.parse('tel:+966-563027472');
-final Uri _url_sms = Uri.parse('sms:+966-563027472');
-
-Future<void> _launchUrl_email() async {
-  if (!await launchUrl(_url_email)) {
-    throw 'Could not launch $_url_email';
-  }
-}
-
-Future<void> _launchUrl_phone() async {
-  if (!await launchUrl(_url_phone)) {
-    throw 'Could not launch $_url_phone';
-  }
-}
-
-Future<void> _launchUrl_web() async {
-  if (!await launchUrl(website)) {
-    throw 'Could not launch $website';
-  }
-}
-
-Future<void> _launchUrl_sms() async {
-  if (!await launchUrl(_url_sms)) {
-    throw 'Could not launch $_url_sms';
-  }
-}
-
 Future<void> _launchUrl_url(type, data) async {
   String url = type + data;
   final Uri url_launcher = Uri.parse(url);
   if (!await launchUrl(url_launcher)) {
-    throw 'Could not launch $_url_sms';
+    throw 'Could not launch $url_launcher';
   }
 }
 
-class contatc extends StatelessWidget {
+class contatc extends StatefulWidget {
   const contatc({Key? key}) : super(key: key);
+
+  @override
+  State<contatc> createState() => _contatcState();
+}
+
+class _contatcState extends State<contatc> {
+  Future<void> _dialogBuilder_add_stories(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('إنشاء ستوري جديد'),
+          content: Form(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: 'title'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'icon_url'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'text_to_show'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'type_url'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'url'),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          )),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('غلق'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('إضافة '),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,66 +122,59 @@ class contatc extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                contact_tile(
-                  title: 'الموقع الإلكتروني ',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: 'youtube',
-                  url: 'https://www.youtube.com/watch?v=cFi0PfYbEI0',
-                  url_type: '',
+                SizedBox(
+                  height: 800,
+                  child: ReorderableListView(
+                      onReorder: (oldIndex, newIndex) {},
+                      children: [
+                        contact_tile(
+                          title: 'الموقع الإلكتروني ',
+                          icon:
+                              'https://cdn-icons-png.flaticon.com/512/556/556813.png',
+                          url_to_show: 'youtube',
+                          url: 'https://www.youtube.com/watch?v=cFi0PfYbEI0',
+                          url_type: '',
+                        ),
+                        contact_tile(
+                          title: 'البريد الإلكتروني',
+                          icon:
+                              'https://cdn-icons-png.flaticon.com/512/556/556813.png',
+                          url_to_show: 'S7QURAN-S@hotmail.com',
+                          url: 'S7QURAN-S@hotmail.com',
+                          url_type: 'mailto:',
+                        ),
+                        contact_tile(
+                          title: 'رقم الهاتف',
+                          icon:
+                              'https://cdn-icons-png.flaticon.com/512/556/556813.png',
+                          url_to_show: '+966-56-302-7472',
+                          url: '+966-56-302-7472',
+                          url_type: 'tel:',
+                        ),
+                        contact_tile(
+                          title: 'الرسائل القصيرة',
+                          icon:
+                              'https://cdn-icons-png.flaticon.com/512/556/556813.png',
+                          url_to_show: '+966-56-302-7472',
+                          url: '+966-56-302-7472',
+                          url_type: 'sms:',
+                        ),
+                        contact_tile(
+                          title: 'إنستغرام',
+                          icon:
+                              'https://th.bing.com/th/id/R.26d9974a1feec9905a4e0d5e5ddf8db6?rik=ycoXFwG5Udz08A&pid=ImgRaw&r=0',
+                          url_to_show: 'prof bilal',
+                          url: 'https://www.instagram.com/prof.billal.4am/',
+                          url_type: '',
+                        ),
+                      ]),
                 ),
-                contact_tile(
-                  title: 'البريد الإلكتروني',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: 'S7QURAN-S@hotmail.com',
-                  url: 'S7QURAN-S@hotmail.com',
-                  url_type: 'mailto:',
-                ),
-                contact_tile(
-                  title: 'رقم الهاتف',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: '+966-56-302-7472',
-                  url: '+966-56-302-7472',
-                  url_type: 'tel:',
-                ),
-                contact_tile(
-                  title: 'الرسائل القصيرة',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: '+966-56-302-7472',
-                  url: '+966-56-302-7472',
-                  url_type: 'sms:',
-                ),
-                contact_tile(
-                  title: 'الرسائل القصيرة',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: '+966-56-302-7472',
-                  url: '+966-56-302-7472',
-                  url_type: 'sms:',
-                ),
-                contact_tile(
-                  title: 'الرسائل القصيرة',
-                  icon: 'https://cdn-icons-png.flaticon.com/512/556/556813.png',
-                  url_to_show: '+966-56-302-7472',
-                  url: '+966-56-302-7472',
-                  url_type: 'sms:',
-                ),
-                contact_tile(
-                  title: 'إنستغرام',
-                  icon:
-                      'https://th.bing.com/th/id/R.26d9974a1feec9905a4e0d5e5ddf8db6?rik=ycoXFwG5Udz08A&pid=ImgRaw&r=0',
-                  url_to_show: 'prof bilal',
-                  url: 'https://www.instagram.com/prof.billal.4am/',
-                  url_type: '',
-                ),
-                Text(
-                  ' إنستغرام',
-                ),
-                Card(
-                    child: ListTile(
-                  onTap: _launchUrl_sms,
-                  leading: Icon(Icons.facebook_outlined),
-                  title: Text('الأستاذ بلال'),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                )),
+                ElevatedButton(
+                  child: Text('add_new'),
+                  onPressed: () {
+                    _dialogBuilder_add_stories(context);
+                  },
+                )
               ],
             ),
           ),
@@ -160,7 +215,13 @@ class contact_tile extends StatelessWidget {
               backgroundImage: NetworkImage(icon),
             ),
             title: Text(url_to_show),
-            trailing: Icon(Icons.arrow_forward_ios),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.remove_circle,
+                color: Colors.red,
+              ),
+              onPressed: () {},
+            ),
           )),
         ],
       ),
