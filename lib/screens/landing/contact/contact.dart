1dbe1704b87442d13a7402ba2ebc5fa45a587bf5
developython14 <djmel1955@gmail.com>
2023-04-18 00:10:26 +0200

    work on crud api

--- lib/screens/landing/contact/contact.dart
+++ lib/screens/landing/contact/contact.dart
@@ -39,10 +39,10 @@ class _contatcState extends State<contatc> {
     final headers = {'Content-type': 'multipart/form-data'};
     request.headers.addAll(headers);
     request.fields.addAll(datatosend);
-    final photo = http.MultipartFile.fromBytes(
-        'icon_title', await cv!.readAsBytes(),
-        filename: cv!.path.split("/").last);
-    request.files.add(photo);
+    //final photo = http.MultipartFile.fromBytes(
+    //'icon_title', await cv!.readAsBytes(),
+    //filename: cv!.path.split("/").last);
+    //request.files.add(photo);
     var push = await request.send();
     var response = await http.Response.fromStream(push);
     var jsonResponse = convert.jsonDecode(response.body);
