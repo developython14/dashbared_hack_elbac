7c3ce29b0080ffe28bd6f92f79191ba778d0a7ff
developython14 <djmel1955@gmail.com>
2023-05-06 06:12:40 +0200

    story PUB

--- lib/screens/landing/Home/componanats/edit_stories.dart
+++ lib/screens/landing/Home/componanats/edit_stories.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 import 'package:future_progress_dialog/future_progress_dialog.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 
 Future<void> _launchUrl_web(url) async {
   final Uri website = Uri.parse(url);
@@ -22,6 +24,8 @@ class edit_stories extends StatefulWidget {
 
 class _edit_storiesState extends State<edit_stories> {
   String? new_title;
+  List<File>? files_stories_added;
+
   Future<void> edit_title_storie() async {
     final url =
         Uri.parse('https://servicessaudi.de.r.appspot.com/put_stories/');
@@ -32,6 +36,34 @@ class _edit_storiesState extends State<edit_stories> {
       'id': context.read<Storiesproviderd>().removed_id_stories.toString(),
       'title': new_title.toString()
     });
+    var push = await request.send();
+    var response = await http.Response.fromStream(push);
+    print(response.body);
+
+    var jsonResponse = convert.jsonDecode(response.body);
+  }
+
+  Future<void> add_new_item() async {
+    final url =
+        Uri.parse('https://servicessaudi.de.r.appspot.com/put_stories/');
+    var request = http.MultipartRequest('POST', url);
+    final headers = {'Content-type': 'multipart/form-data'};
+    request.headers.addAll(headers);
+    request.fields.addAll({
+      'id': context.read<Storiesproviderd>().removed_id_stories.toString(),
+    });
+    for (var el in files_stories_added!) {
+      try {
+        final photo = http.MultipartFile.fromBytes(
+            el.toString(), await el!.readAsBytes(),
+            filename: el!.path.split("/").last);
+        request.files.add(photo);
+      } catch (e) {
+        print('KAYN ERROR');
+        print(e);
+      }
+    }
+
     var push = await request.send();
     var response = await http.Response.fromStream(push);
     print(response.body);
@@ -91,7 +123,32 @@ class _edit_storiesState extends State<edit_stories> {
                             ),
                           ),
                         ))
-                    .toList())
+                    .toList()),
+            GestureDetector(
+              onTap: () async {
+                FilePickerResult? result =
+                    await FilePicker.platform.pickFiles(allowMultiple: true);
+
+                if (result != null) {
+                  setState(() {
+                    files_stories_added =
+                        result.paths.map((path) => File(path!)).toList();
+                  });
+                } else {
+                  // User canceled the picker
+                }
+              },
+              child: Text('add new stories item'),
+            ),
+            TextButton(
+                onPressed: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (context) => FutureProgressDialog(add_new_item(),
+                        message: Text('Loading...')),
+                  );
+                },
+                child: Text('upload files'))
           ],
         )),
       ),
